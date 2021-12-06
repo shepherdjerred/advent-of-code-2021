134dddcc25f0484b6386c5b687d0b8df0b8096a6
Jerred Shepherd <shepherdjerred@gmail.com>
2021-12-06 09:26:18 -0800

    Parse input

--- Day5/Main.hs
+++ Day5/Main.hs
@@ -1,5 +1,37 @@
 module Main where
 
+import Data.List
+import Data.List.Split
+
+data Point = Point { x :: Int, y :: Int, count :: Int } deriving (Show, Eq)
+type Line = (Point, Point)
+type Grid = [[(Point)]]
+
+countIntersections :: Int -> Grid -> Int
+countIntersections = undefined
+
+applyLine :: Line -> Grid -> Grid
+applyAnswer line grid = 
+
+populate :: [Line] -> Grid -> Grid
+populate [] grid = grid
+
+readInt :: String -> Int
+readInt = read
+
+parsePoint :: String -> Point
+parsePoint input = Point (head coords) (last coords) 0
+    where coords = fmap readInt $ splitOn "," input
+
+parseLine :: String -> Line
+parseLine input = ((head points), (last points))
+    where points = fmap (parsePoint) $ splitOn "->" input
+
+parse :: [String] -> [Line]
+parse = fmap parseLine
+
 main :: IO ()
 main = do
-    print "Hello world!"
+    text <- readFile "resources/Day5.txt"
+    let input = parse $ lines text
+    print input
