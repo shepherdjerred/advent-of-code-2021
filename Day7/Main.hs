db96a336d4f5aa2bc5984c2a466f4d62174a93b0
Jerred Shepherd <shepherdjerred@gmail.com>
2021-12-12 22:44:54 -0800

    Day 7, Part 1

--- Day7/Main.hs
+++ Day7/Main.hs
@@ -1,5 +1,30 @@
 module Main where
 
+import Data.List.Split
+import Data.List
+
+parse :: String -> [Int]
+parse input = fmap read $ splitOn "," input
+
+absolute :: Int -> Int
+absolute i
+  | i > 0 = i
+  | otherwise = -i
+
+-- Finds the median of a sorted list
+median :: [Int] -> Int
+median nums
+  | odd = nums!!midpoint
+  | otherwise = ((nums!!midpoint) + (nums!!(midpoint - 1))) `div` 2
+  where len = length nums
+        odd = len `mod` 2 == 1
+        midpoint = len `div` 2
+
+distance :: Int -> Int -> Int
+distance median number = absolute(median - number)
+
 main :: IO ()
 main = do
-  print "Hello world!"
+  text <- readFile "resources/Day7.txt"
+  let crabs = sort $ parse text
+  print $ sum $ fmap (distance (median crabs)) crabs
