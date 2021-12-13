ac27391be520d0b44d5a4a800b9761d6ce73911d
Jerred Shepherd <shepherdjerred@gmail.com>
2021-12-12 19:29:29 -0800

    Day 6, Part 1

--- Day6/Main.hs
+++ Day6/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import Data.List.Split
+
+simulateDays :: Int -> [Int] -> [Int]
+simulateDays days fish
+  | days == 0 = fish
+  | otherwise = simulateDays (days - 1) (concat $ fmap simulate fish)
+
+simulate :: Int -> [Int]
+simulate i
+  | i == 0 = [6, 8]
+  | otherwise = [i - 1] 
+
+readInt :: String -> Int
+readInt = read
+
+parse :: String -> [Int]
+parse input = fmap readInt $ splitOn "," input
+
 main :: IO ()
 main = do
-    print "Hello world!"
+    text <- readFile "resources/Day6.txt"
+    let fish = parse text
+    let end = simulateDays 80 fish
+    print $ length end
